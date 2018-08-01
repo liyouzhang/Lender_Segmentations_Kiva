70a07dc2ec21bc4cbd3e460bbc8675a06cbbfe9a
Liyou Zhang <liyou713@gmail.com>
2018-08-01 16:32:48 -0700

    update script for pandas svd model

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import scipy.stats as sts
 %matplotlib inline
 
@@ -9,10 +9,10 @@ import pandas as pd
 from sklearn.decomposition import NMF
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem.snowball import SnowballStemmer
-from nltk.stem.wordnet import WordNetLemmatizer
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize
+# from nltk.stem.snowball import SnowballStemmer
+# from nltk.stem.wordnet import WordNetLemmatizer
 import string
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import svds
@@ -24,7 +24,8 @@ from scipy.sparse.linalg import svds
 
 loans_lenders = pd.read_csv('kiva_ds_csv/loans_lenders.csv')
 loans_lenders["LENDERS_lst"] = loans_lenders["LENDERS"].map(lambda x: x.split(','))
-loans_lenders_drop = loans_lenders.drop('LENDERS',axis=1)
+ll = loans_lenders.copy()
+loans_lenders = loans_lenders.drop('LENDERS',axis=1)
 
 lst_col = 'LENDERS_lst'
 df_ll = pd.DataFrame({col:np.repeat(loans_lenders[col].values, loans_lenders[lst_col].str.len())for col in loans_lenders.columns.difference([lst_col])}).assign(**{lst_col:np.concatenate(loans_lenders[lst_col].values)})[loans_lenders.columns.tolist()]
