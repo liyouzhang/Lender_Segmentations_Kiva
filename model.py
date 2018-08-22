8a7eff179465bda11ee6aa5cc81a795182b63d77
Liyou Zhang <liyou713@gmail.com>
2018-08-22 13:30:34 -0700

    add doc strings to model functions

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,7 +14,18 @@ from time import time
 
 
 def PCA_reduce(X,dimensionality):
-    '''return reduced n dimensionality matrix and np array of important features'''
+    '''[summary]
+    reduced n dimensionality matrix and np array of important features
+
+    Arguments:
+        X {[numpy matrix]} -- input matrix
+        dimensionality {[int]} -- to what dimensionality to reduce to
+
+    Returns:
+        X_reduced -- reduced matrix
+        important_features -- a list of features that contributed most to the principle component
+    '''
+
     important_features = []
     pcd = PCA(dimensionality).fit(X)
     pca_matrix = pcd.components_
@@ -30,7 +41,17 @@ def PCA_reduce(X,dimensionality):
     return X_reduced, important_features
 
 def print_imp_features(df,imp_features):
-    '''print important features names and return the counter of the features'''
+    '''[summary]
+    print important features names and return the counter of the features
+
+    Arguments:
+        df {[pandas dataframe]} -- input
+        imp_features {[list]} -- important features returned from pca_reduce function
+
+    Returns:
+        counter [dictionary] -- a dictionary with feature name as key, and frequency showed as value 
+    '''
+
     feature = []
     idx = 0
     c = 1
@@ -42,66 +63,30 @@ def print_imp_features(df,imp_features):
                 print(list(df.columns)[i])
         else:
             for i in np.array(imp_features).flatten()[idx:idx+5]:
-                feature.append(list(df.columns)[i])        
+                feature.append(list(df.columns)[i])
                 print(list(df.columns)[i])
         idx += 5
         c +=1
-    counter = Counter(feature)    
+    counter = Counter(feature)
     return counter
 
 
 def kmeans_cluster(X_reduced,cluster_num):
-    '''return y (assigned_cluster for each row) and centers of the clusters '''
+    '''[summary]
+    use kmeans++ algorithems to cluster input data.
+
+    Arguments:
+        X_reduced {[numpy matrix]} -- input matrix
+        cluster_num {[int]} -- how many clusters for clustering 
+
+    Returns:
+        y[numpy array] -- assigned_cluster for each row
+        centers[numpy matrix] -- centers of the clusters
+    '''
+
     kmeans = KMeans(init='k-means++', n_clusters=cluster_num, n_init=10,tol=0.0001,verbose=0)
     y = kmeans.fit_predict(X_reduced)
     centers = kmeans.cluster_centers_
     # assigned_cluster = kmeans.transform(X_reduced).argmin(axis=1)
     return y, centers
 
-# def drop_outliers(df,outlier_index_lst=[2987,27627,15038,19433,704]):
-#     '''drop the outliers identifed from PCA and Kmeans'''
-#     df = df.drop(df.index[outlier_index_lst])
-#     return df
-
-
-def make_dendrogram(X, linkage_method, metric, figsize=(25,15),color_threshold=None):
-    '''
-    This function creates and plots the dendrogram created by hierarchical clustering.
-    
-    INPUTS: Pandas Dataframe, string, string, int
-    
-    OUTPUTS: None
-    '''
-    distxy = squareform(pdist(X, metric=metric))
-    Z = linkage(distxy, linkage_method)
-    plt.figure(figsize=figsize)
-    plt.title('Hierarchical Clustering Dendrogram')
-    plt.xlabel('sample index')
-    plt.ylabel('distance')
-    dendrogram(
-        Z,
-        leaf_rotation=90.,  # rotates the x axis labels
-        leaf_font_size=12.,  # font size for the x axis labels
-#         labels = dataframe.index,
-        color_threshold = color_threshold
-    )
-    plt.show()
-
-
-def DBSCAN_cluster(X,eps=1,min_samples=10):
-    '''return labels and n_clusters'''
-    db = DBSCAN(eps=eps, min_samples=min_samples).fit(X)
-    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-    core_samples_mask[db.core_sample_indices_] = True
-    labels = db.labels_
-    # Number of clusters in labels, ignoring noise if present.
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-
-    return labels,n_clusters_
-
-def lle_dimensionality_reduction(X, n_neighbors, n_dimensionality):
-    time0=time()
-    X_lle,err = locally_linear_embedding(X,n_neighbors,n_dimensionality)
-    time1=time()
-    print(time1-time0)
-    return X_lle,err
